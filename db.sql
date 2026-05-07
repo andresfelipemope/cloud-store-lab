9d4a65a5537af7cbd25f915316fb3e8744fce0d9
Andres Monsalve <andresfelipemope@ufps.edu.co>
2026-05-07 15:42:24 -0500

    Archivos para conectarse a la db y usar script

---
+++ db.sql
@@ -0,0 +1,14 @@
+-- CREATE TABLE IF NOT EXISTS product(
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     price DECIMAL(10, 2) NOT NULL,
+--     description VARCHAR(255),
+--     img_url VARCHAR(2083),
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+INSERT INTO product(
+    name, price, description
+) VALUES(
+    'Dieta salchipapa', 199.69, 'Lo mejor para tu chanticos(perros)'
+)
---
+++ init_db.py
@@ -0,0 +1,34 @@
+import os
+import psycopg2
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+conn = psycopg2.connect(
+    host=DB_HOST,
+    port=DB_PORT,
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD
+)
+
+cursor = conn.cursor()
+
+with open("./db.sql", "r") as file:
+    sql = file.read()
+
+cursor.execute(sql)
+
+conn.commit()
+
+cursor.close()
+conn.close()
+
+print("Database initialized successfully.")
